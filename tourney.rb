1bf71da217876a74e7d59940ab5116c31b602519
SuperAnalizador <89427089+SuperAnalizador@users.noreply.github.com>
2021-09-09 20:15:23 -0300

    Add dnisearch
    tourney.rb  ok
    tourney_spec.rb ok
    add def dnisearch

--- tourney.rb
+++ tourney.rb
@@ -6,4 +6,15 @@ class Tourney
         @competitors = Array.new
     end
 end
+    
+    def dnisearch(dni)
+        for competitor in @competitors
+            if competitor.dni == dni
+                return competitor
+            puts "Participante de Código: #{competitor.code} DNI: #{competitor.dni} Nombre: #{competitor.name} Edad: #{competitor.age} años. Puntaje: #{competitor.score}"
+            else
+            puts "Participante no encontrado"
+        end
+    end
+end
 
--- tourney_spec.rb
+++ tourney_spec.rb
@@ -2,8 +2,18 @@ require_relative 'tourney'
 require_relative 'competitor'
 require_relative 'team'
 RSpec.describe Tourney do
-    it 'initialize tourney class' do
-    tourney = Tourney.new
-    expect(tourney).to be_an_instance_of(Tourney)
+    context 'initialize team class' do
+        it 'new#tourney' do
+            @teams = Tourney.new
+            expect(@teams).to be_an_instance_of(Tourney)
+        end
+        it 'new#tourney' do
+            @competitors = Tourney.new
+            expect(@competitors).to be_an_instance_of(Tourney)
+        end
+        it '#dnisearch' do
+            @competitors = Competitor.new 'P001', 'Juan Diaz', 22, '12345678', 'black', 5, 0, 0
+            expect(@competitors.dni).to eq('12345678')
+        end
     end
 end
